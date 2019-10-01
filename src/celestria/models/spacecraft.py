9f829899dde50a3b056ebce4d87beb83f4d56a01
embersabsalon <embersabsalon@protonmail.com>
2019-10-01 15:35:08 +0800

    Initialize spacecraft model

---
+++ src/celestria/models/spacecraft.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from authorizing_body import AuthorizingBody
+
+
+class SpaceCraft(models.Model):
+    name = models.CharField(max_length=250)
+    passenger_capacity = models.PositiveIntegerField()
+    registering_body = models.ForeignKey(AuthorizingBody)
