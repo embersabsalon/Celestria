501f73daa4a80c7b8f170c01be2290b1e76be6ab
embersabsalon <embersabsalon@protonmail.com>
2019-10-08 15:17:53 +0800

    Add personal details for customer model

--- src/celestria/models/customer.py
+++ src/celestria/models/customer.py
@@ -17,3 +17,21 @@ class Customer(models.Model):
         AccountProfile, related_name='customers')
     type = models.IntegerField(
         choices=TYPE_CHOICES, default=REGULAR)
+    first_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255)
+    birth_date = models.DateField(blank=True, null=True)
+
+    @property
+    def fullname(self):
+        if self.middle_name:
+            return '{} {} {}'.format(
+                self.first_name, self.middle_name, self.last_name)
+        return '{} {}'.format(
+            self.first_name, self.last_name)
+
+    @property
+    def age(self):
+        if self.birth_date:
+            return relativedelta(date.today() - self.birth_date).years
+        return None
