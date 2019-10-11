95d720c83142b5bf1970eebec903cc5bfc7773ce
embersabsalon <embersabsalon@protonmail.com>
2019-10-11 16:59:57 +0800

    Add ticket model

---
+++ src/celestria/models/ticket.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from spacecraft import SpaceCraft
+from spaceflight import SpaceFlight
+from account_profile import AccountProfile
+
+
+class Ticket(models.Model):
+    SELLING = 0
+    RESERVED = 1
+    BOARDED = 2
+    CANCELLED = 3
+    REFUNDED = 4
+
+    STATUS_CHOICES = (
+        (SELLING, 'Selling'),
+        (RESERVED, 'Reserved'),
+        (BOARDED, 'Boarded'),
+        (CANCELLED, 'Cancelled'),
+        (REFUNDED, 'Refunded'),
+    )
+
+    number = models.CharField(max_length=250)
+    flight = models.ForeignKey(
+        SpaceFlight, related_name='tickets')
+    sold_to = models.OneToOneField(
+        AccountProfile, related_name='tickets_bought')
+    seat_number = models.CharField(max_length=64)
+    status = models.IntegerField(
+        choices=STATUS_CHOICES, default=SELLING)
+    actual_price = models.DecimalField(
+        default=0, max_digits=32,
+        decimal_places=2, null=True, blank=True)
+    sold_on = models.DateTimeField(
+        null=True, blank=True)
+    boarded_on = models.DateTimeField(
+        null=True, blank=True)
+    cancelled_on = models.DateTimeField(
+        null=True, blank=True)
+    refunded_on = models.DateTimeField(
+        null=True, blank=True)
