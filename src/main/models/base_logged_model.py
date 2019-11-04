aa18bf30beb8548a3990067f72f4f30132645030
embersabsalon <embersabsalon@protonmail.com>
2019-11-04 11:01:05 +0800

    Remove modified by field

--- src/main/models/base_logged_model.py
+++ src/main/models/base_logged_model.py
@@ -6,17 +6,13 @@ from django.utils import timezone
 
 class BaseLoggedModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
-    creator = models.ForeignKey(
-        User, null=True, blank=True, related_name='created_objects')
+    creator = models.ForeignKey(User, null=True, blank=True)
     modified_on = models.DateTimeField(auto_now=True)
-    modified_by = models.ForeignKey(
-        User, null=True, blank=True, related_name='modified_objects')
 
     def save(self, *args, **kwargs):
         if self.pk is None and hasattr(local, 'user'):
             self.creator = local.user
-        self.modified = local.user
-        modified = timezone.now()
+        self.modified = timezone.now()
         return super(BaseLoggedModel, self).save(*args, **kwargs)
 
     class Meta:
