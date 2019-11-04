2488ec9f4c5d21f0ed646af6f21211803a4f3b8d
embersabsalon <embersabsalon@protonmail.com>
2019-11-04 10:43:15 +0800

    Fix import error of logging middleware

--- src/main/settings.py
+++ src/main/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.logging.LoggingMiddleware'
+    'main.middlewares.logging.LoggingMiddleware'
 ]
 
 ROOT_URLCONF = 'main.urls'
