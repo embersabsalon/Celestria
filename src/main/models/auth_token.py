9b5e5939d9ebc6f3f1b140d06d35dbe26c3b91b2
embersabsalon <embersabsalon@protonmail.com>
2019-11-26 14:25:06 +0800

    Make token code a default uuid4 for auth tokens

--- src/main/models/auth_token.py
+++ src/main/models/auth_token.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class AuthenticationToken(models.Model):
     user = models.ForeignKey(User)
-    token = models.CharField(max_length=255)
+    token = models.CharField(max_length=255, default=uuid4)
     expiration_date = models.DateTimeField()
